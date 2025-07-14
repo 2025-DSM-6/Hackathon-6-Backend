6864fe6fee5844523ae42c349d675d7c84ca8458
민수아 <Minsua0503@gmail.com>
2025-07-15 08:32:39 +0900

    chore :: 프로젝트 세팅

---
+++ src/main/java/com/example/hackathon6backend/Hackathon6BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.hackathon6backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackathon6BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hackathon6BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hackathon6backend/Hackathon6BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hackathon6backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackathon6BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
