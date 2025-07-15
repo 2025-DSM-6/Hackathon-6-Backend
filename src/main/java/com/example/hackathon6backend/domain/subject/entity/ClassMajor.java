fa0c6f3ffa0de3afd5da38996ab50862b24bb280
민수아 <Minsua0503@gmail.com>
2025-07-16 08:30:17 +0900

    refactor: 이넘값 리팩토링

--- src/main/java/com/example/hackathon6backend/domain/subject/entity/ClassMajor.java
+++ src/main/java/com/example/hackathon6backend/domain/subject/entity/ClassMajor.java
@@ -1,8 +1,15 @@
 package com.example.hackathon6backend.domain.subject.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum ClassMajor {
-    all,
-    software,
-    embedded,
-    artificial_intelligence
+    COMMON("공통교육과정"),
+    SOFTWARE("소프트웨어개발과"),
+    EMBEDDED("임베디드소프트웨어과"),
+    AI("인공지능소프트웨어과");
+
+    private final String name;
 } 
--- src/main/java/com/example/hackathon6backend/domain/subject/entity/ElectiveSubject.java
+++ src/main/java/com/example/hackathon6backend/domain/subject/entity/ElectiveSubject.java
@@ -1,8 +1,15 @@
 package com.example.hackathon6backend.domain.subject.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum ElectiveSubject {
-    server_dev,
-    front_dev,
-    linux_dev,
-    AI_use
+    SERVER_DEV("서버 프로그래밍"),
+    FRONT_DEV("프론트웨어 프로그래밍"),
+    LINUX_DEV("리눅스 프로그래밍"),
+    AI_USE("인공지능 활용");
+
+    private final String name;
 } 
