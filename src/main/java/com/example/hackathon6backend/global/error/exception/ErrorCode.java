06b60f3623f7532a0c418960893d2a6cc4b7ba5f
민수아 <Minsua0503@gmail.com>
2025-07-15 09:16:37 +0900

    feat(#1): ErrorCode

---
+++ src/main/java/com/example/hackathon6backend/global/error/exception/ErrorCode.java
@@ -0,0 +1,14 @@
+package com.example.hackathon6backend.global.error.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error");
+
+    private final HttpStatus status;
+    private final String message;
+}
