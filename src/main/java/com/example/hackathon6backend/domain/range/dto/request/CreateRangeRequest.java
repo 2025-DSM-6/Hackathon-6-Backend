aabe0921a05ff9d977830b7e687e88d6285e4e57
kimsee8200 <ttungseui11@gmail.com>
2025-07-16 11:06:16 +0900

    refactor(#8): 시험범위 앤티티 변수 추가에 따른 DTO 변경

--- src/main/java/com/example/hackathon6backend/domain/range/dto/request/CreateRangeRequest.java
+++ src/main/java/com/example/hackathon6backend/domain/range/dto/request/CreateRangeRequest.java
@@ -1,6 +1,5 @@
 package com.example.hackathon6backend.domain.range.dto.request;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,9 +8,6 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 public class CreateRangeRequest {
-    
     private String memo;
-    
-    @NotNull(message = "시험범위를 입력해주세요.")
-    private List<String> examRange;
+    private List<RangeContentRequest> examRange;
 } 
