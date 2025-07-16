d54c59e86a6a738ef4f50f10d2d12ecc20f30e3d
kimsee8200 <ttungseui11@gmail.com>
2025-07-16 12:01:41 +0900

    refactor(#8): resolve conflict -> ErrorCode.java

--- src/main/java/com/example/hackathon6backend/global/error/exception/ErrorCode.java
+++ src/main/java/com/example/hackathon6backend/global/error/exception/ErrorCode.java
@@ -8,8 +8,6 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error"),
-<<<<<<< HEAD
-    
     // 공통 예외
     FORBIDDEN(HttpStatus.FORBIDDEN, "권한이 없습니다."),
     
@@ -21,13 +19,11 @@ public enum ErrorCode {
     INVALID_RANGE_ACCESS(HttpStatus.FORBIDDEN, "해당 시험범위에 접근 권한이 없습니다."),
     EMPTY_RANGE_CONTENT(HttpStatus.BAD_REQUEST, "시험범위 내용이 비어있습니다."),
     DUPLICATE_RANGE_EXISTS(HttpStatus.CONFLICT, "이미 등록된 시험범위가 있습니다."),
-    CLASS_NOT_FOUND(HttpStatus.NOT_FOUND, "반 정보를 찾을 수 없습니다.");
-=======
+    CLASS_NOT_FOUND(HttpStatus.NOT_FOUND, "반 정보를 찾을 수 없습니다."),
 
     // jwt
     EXPIRED_JWT(HttpStatus.UNAUTHORIZED, "Expired JWT"),
     INVALID_JWT(HttpStatus.UNAUTHORIZED, "Invalid JWT");
->>>>>>> origin/main
 
     private final HttpStatus status;
     private final String message;
