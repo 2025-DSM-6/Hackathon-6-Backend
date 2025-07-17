c146790d30aff6df98648c5629e74248ce972ceb
민수아 <Minsua0503@gmail.com>
2025-07-17 10:11:59 +0900

    feat(#19): findAllByUsername 쿼리 메서드 추가

--- src/main/java/com/example/hackathon6backend/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/hackathon6backend/domain/user/repository/UserRepository.java
@@ -4,9 +4,11 @@ import com.example.hackathon6backend.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByAccountId(String accountId);
+    List<User> findAllByUsername(String username);
 }
